ab45f5a5c6eef1b975dfc93dfc497a8876d05f16
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-02-10 12:21:44 -0300

    - res.sendFile() to serve static homepage html

--- expressServer.js
+++ expressServer.js
@@ -4,6 +4,8 @@
 // creating a robust API is quick and easy.
 // Ref.: https://expressjs.com/
 
+const path = require('path');
+
 const express = require('express');
 
 const SERVER_PORT = 3000;
@@ -12,13 +14,24 @@ const SERVER_PORT = 3000;
 // invoked and is an Express application
 const app = express();
 
+// serve up static files with only 1 line!! :-)
+app.use(express.static('public'));
+
 // "all" is a method that takes 2 args:
 // 1. route
 // 2. callback to run if the route is requested
-app.all('*', (req, res) => {
+app.all('/', (req, res) => {
   // Express handles all the basic headers for us (status code, mime-type)
-  res.send('<h1>Welcome to our home page!</h1>')
+  // read in node.html
+  console.log(path.join(__dirname + '/node.html'));
+  res.sendFile(path.join(__dirname + '/node.html'));
+  
+  //res.send('<h1>Welcome to our home page!</h1>')
   // Express also handles the end() to close the http connection
+});
+
+app.all('*', (req, res) => {
+  res.send('<h1>Sorry, this page does not exist!</h1>');
 });
 
 app.listen(SERVER_PORT);
